d485024086b97a1609c2d1c8403af7802b79dbae
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-07-08 11:43:58 +0500

    Designed the Singup Page.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img class="logo" alt="Vue logo" src="./assets/logo.png" />
+  <SignUp />
+  <div class="register">
+    <input type="text" placeholder="Enter Your Name" />
+    <input type="email" placeholder="Email" />
+    <input type="password" placeholder="Password" />
+
+    <button>
+      Signup
+    </button>
+  </div>
 </template>
 
 <script>
+import SignUp from './components/Signup.vue'
+
 export default {
-  name: 'App',
+  components: {
+    SignUp
+  }
 }
 </script>
 
@@ -17,5 +30,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo {
+  width: 100px;
+  height: 100px;
+  margin: 20px auto;
+}
+.register input {
+  width: 300px;
+  height: 40px;
+  padding-left: 20px;
+  margin-bottom: 30px;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid skyblue;
+  display: block;
+}
+.register button {
+  width: 320px;
+  height: 40px;
+  background: skyblue;
+  color: white;
+  cursor: pointer;
+  border: none;
 }
 </style>
