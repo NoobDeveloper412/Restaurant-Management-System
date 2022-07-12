109091aba50f240aaefc3ad13a4240032f71983e
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-07-12 18:39:54 +0500

    Created the logout functionality by just removing all the data from localStorage.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,13 +3,19 @@
     <a href="/">Home</a>
     <a href="/add">Add Restaurant</a>
     <a href="/update">Update Restaurant</a>
-    <a href="/">Logout</a>
+    <a href="#" v-on:click="logout">Logout</a>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Main-Header'
+  name: 'Main-Header',
+  methods: {
+    logout () {
+      localStorage.clear()
+      this.$router.push('/signin')
+    }
+  }
 }
 </script>
 
