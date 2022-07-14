42cac5c10b4e711b935d367bcd3f2ff133a3bdc3
NoobDeveloper412 <74310120+NoobDeveloper412@users.noreply.github.com>
2022-07-14 13:56:40 +0500

    Fetched the data from the json file, then simply showed them in a simple list.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,28 +2,53 @@
 <template>
   <Header />
   <h1>Hello {{ name }} from Home</h1>
+  <table border="1">
+  <tr>
+  <td>ID</td>
+  <td>Name</td>
+  <td>Contact</td>
+  <td>Address</td>
+  </tr>
+    <tr v-for="item in restaurants" :key="item.id">
+      <td>{{ item?.id }}</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.contact }}</td>
+      <td>{{ item.address }}</td>
+    </tr>
+  </table>
 </template>
 
 <script>
 import Header from './Header.vue'
+import axios from 'axios'
 export default {
   name: 'Home-Page',
   data () {
     return {
-      name: ''
+      name: '',
+      restaurants: []
     }
   },
   components: {
     Header
   },
-  mounted () {
+  async mounted () {
     let user = localStorage.getItem('userInfo')
     this.name = JSON.parse(user)[0].name
     if (!user) {
       this.$router.push({ name: 'SignIn' })
     }
+
+    let result = await axios.get('http://localhost:3000/restaurant')
+    // console.warn(result)
+    this.restaurants = result.data
   }
 }
 </script>
 
-<style></style>
+<style>
+td{
+  width: 160px;
+  height: 40px;
+}
+</style>
